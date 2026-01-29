c32fff097e11b6d8f560bce56cc99d565a540016
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2026-01-29 21:08:23 +0530

    add error-handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
+import type { NextFunction, Request, Response } from 'express';
 import express from 'express';
+
+import type { HttpError } from 'http-errors';
+import createHttpError from 'http-errors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to Auth Service' });
+app.get('/', async (_req, res, next) => {
+    const err = createHttpError(500, 'Something went wrong');
+    next(err);
+    // res.json({ message: 'Welcome to Auth Service' });
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, _req: Request, res: Response, _next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+            },
+        ],
+    });
 });
 
 export default app;
