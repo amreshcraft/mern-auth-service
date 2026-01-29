682df8a28366616bdebdc4fa8101953efbffaca4
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2026-01-29 20:27:43 +0530

    add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 export const Config = {
     PORT,
+
+    NODE_ENV: NODE_ENV || 'dev',
 };
 
 Object.freeze(Config);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import app from './app.js';
 import { Config } from './config/index.js';
+import { logger } from './config/logger.js';
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () => {
-            console.log('server is running on port:', PORT);
+            logger.info(`server is running on port: ${PORT}`);
         });
     } catch (error) {
-        console.error('error:', error);
+        logger.error({ error: error });
         process.exit(1);
     }
 };
